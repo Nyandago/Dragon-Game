dbc08574c32c9a4ca8f65a55b51b8dd8fb797520
nyandago <paulmutator@gmail.com>
2020-09-22 21:56:06 +0300

    Added the movement to the stumps

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+        if (transform.position.x < -15)
+            transform.position += new Vector3(30, 0, 0); //Vector3.right * 30;
+    }
+}

0840056d4677a5b637a04a97d1dce05173b34c31
nyandago <paulmutator@gmail.com>
2020-09-23 10:21:33 +0300

    Added fireball to the scene

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 1;
+    private Enemy enemy;
+
+    private void Awake()
+    {
+        enemy = GetComponent<Enemy>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,7 +20,7 @@ public class MoveLeft : MonoBehaviour
         {
             transform.position += new Vector3(30, 0, 0);
             ShowRandomSprite();
-
+            enemy?.Respawn();
         }
     }
 
